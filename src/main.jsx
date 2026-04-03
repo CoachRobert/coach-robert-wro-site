913229df8f0a18844d2e5758abbcd4cd7c573050
CoachRobert <robertguo1974@gmail.com>
2026-04-02 18:30:04 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App(){
+  return <div style={{padding:20}}>请把你的完整代码粘贴到这里</div>
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
